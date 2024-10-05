9f0ddd321348627b586927bb917cd77d957795b2
Tanveer Singh <tanveerbadhan@gmail.com>
2024-10-05 12:50:33 -0400

    dummy commit

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const app = express();
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
 app.use("/", auth);
 app.use("/", order);
 
