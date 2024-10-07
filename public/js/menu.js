3d33e15ec5ddc2dc58d1b6f2efb2f5e81feed1e8
Tanveer Singh <tanveerbadhan@gmail.com>
2024-10-07 12:40:10 -0400

    added logout implementation

--- public/js/menu.js
+++ public/js/menu.js
@@ -1,7 +1,14 @@
 const logoutBtn = document.querySelector(".logoutBtn");
 
-const handleLogout = () => {
-    console.log("logout clicked");
+const handleLogout = async () => {
+    try {
+        const endpoint = "/logout";
+        const fullUrl = window.location.origin + endpoint;
+        const response = await fetch(fullUrl);
+        window.location.href = window.location.origin;
+    } catch (error) {
+        console.log(error.message);
+    }
 };
 
 logoutBtn.addEventListener("click", handleLogout);
